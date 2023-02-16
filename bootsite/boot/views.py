e217d29ef233df3ffbad90f4e5dd234d60174a4d
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-02-16 11:20:11 +0300

    #3_________РАЗДЕЛ САЙТА О НАС,

--- bootsite/boot/views.py
+++ bootsite/boot/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'boot/index.html')
+    about = [
+        {'icon': 'icon-list-numbered', 'text': 'Структурированный материал'},
+        {'icon': 'icon-rouble', 'text': 'Cимволическая цена'},
+        {'icon': 'icon-chart-bar', 'text': 'Пожизненный доступ'},
+        {'icon': 'icon-code', 'text': 'Практические задания'},
+        {'icon': 'icon-file-video', 'text': '35 видео уроков (5 часов)'},
+        {'icon': 'icon-mobile', 'text': 'Просмотр со смартфона'},
+
+    ]
+    return render(request, 'boot/index.html', {'about': about})
