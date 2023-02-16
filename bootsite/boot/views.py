f6c9c6ff26a016edf2816d95f8497a88e1457fe1
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-02-16 12:46:03 +0300

    #4_________СЕТКА 2 НА 2

--- bootsite/boot/views.py
+++ bootsite/boot/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 
-def index(request):
-    about = [
+about = [
         {'icon': 'icon-list-numbered', 'text': 'Структурированный материал'},
         {'icon': 'icon-rouble', 'text': 'Cимволическая цена'},
         {'icon': 'icon-chart-bar', 'text': 'Пожизненный доступ'},
         {'icon': 'icon-code', 'text': 'Практические задания'},
         {'icon': 'icon-file-video', 'text': '35 видео уроков (5 часов)'},
         {'icon': 'icon-mobile', 'text': 'Просмотр со смартфона'},
-
     ]
-    return render(request, 'boot/index.html', {'about': about})
+forwhom = [
+    {'text': 'Хочет научиться создавать сайты своими силами'},
+    {'text': 'Учится современным инструментам разработки'},
+    {'text': 'Ещё не знает язык HTML и CSS, но хочет выучить'},
+    {'text': 'Хочет стать крутым мастером и зарабатывать деньги'},
+]
+def index(request):
+    return render(request, 'boot/index.html', {'about': about, 'whom': forwhom})
