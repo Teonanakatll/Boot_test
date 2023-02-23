11014a2bcb7f10f5b330ef46cd3007dc10687822
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-02-23 11:48:16 +0300

    #10________СОЗДАНИЕ ПРИЛОЖЕНИЯ java

--- bootsite/bootsite/settings.py
+++ bootsite/bootsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'boot.apps.BootConfig',
+    'java.apps.JavaConfig',
 ]
 
 MIDDLEWARE = [
--- bootsite/bootsite/urls.py
+++ bootsite/bootsite/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('boot.urls')),
+    path('java/', include('java.urls')),
 ]
