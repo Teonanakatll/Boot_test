dbe149e45d912b9e0d51956dc5385aef93a1f0d8
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-03-03 19:49:55 +0300

    17_______footer УРАААА Я СДЕЛАЛ ЭТО!!!

--- bootsite/java/views.py
+++ bootsite/java/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 from .models import Chapter
 
+foot = ['icon-youtube-play',
+        'icon-paper-plane',
+        'icon-vkontakte',
+        'icon-instagram',
+        'icon-github-circled'
+        ]
 
 def index2(request):
     chapt = Chapter.objects.filter(pk__lt=5)
-    return render(request, 'java/index2.html', {'chapt': chapt})
+    return render(request, 'java/index2.html', {'foot': foot, 'chapt': chapt})
