532adea9cb347aae942fae953c6afc0e6a7fd7fb
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-08-29 18:53:46 +0300

    find_dotenv

--- bootsite/bootsite/settings.py
+++ bootsite/bootsite/settings.py
@@ -13,7 +13,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 
-load_dotenv(find_dotenv)
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
